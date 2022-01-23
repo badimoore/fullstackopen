d3a697c3feba7cce48997a3603faf13173885e7d
Alexander Moore <badi_moore@hotmail.com>
2022-01-23 23:04:56 +0200

    1.8 valmis

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,6 +4,26 @@ const Button = ({ handler, text }) => <button onClick={handler}>{text}</button>
 
 const Stat = ({ name, value }) => <div>{name} {value}</div>
 
+const Statistics = props => {
+  const [good, neutral, bad] = props.values
+
+  const total = good + neutral + bad;
+  const percentGood = () => total > 0 ? good / total * 100 + ' %' : '-'
+  const average = () => total > 0 ? (good - bad) / total : '-'
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <Stat name='good' value={good} />
+      <Stat name='neutral' value={neutral} />
+      <Stat name='bad' value={bad} />
+      <Stat name='all' value={total} />
+      <Stat name='average' value={average()} />
+      <Stat name='positive' value={percentGood()} />
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -13,11 +33,6 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
 
-  const total = good + neutral + bad;
-
-  const percentGood = () => total > 0 ? good / total * 100 + ' %' : '-'
-  const average = () => total > 0 ? (good - bad) / total : '-'
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -25,14 +40,7 @@ const App = () => {
       <Button handler={handleNeutral} text='neutral' />
       <Button handler={handleBad} text='bad' />
 
-      <h1>statistics</h1>
-      <Stat name='good' value={good} />
-      <Stat name='neutral' value={neutral} />
-      <Stat name='bad' value={bad} />
-      <Stat name='all' value={total} />
-      <Stat name='average' value={average()} />
-      <Stat name='positive' value={percentGood()} />
-
+      <Statistics values={[good, neutral, bad]} />
     </div>
   );
 }
