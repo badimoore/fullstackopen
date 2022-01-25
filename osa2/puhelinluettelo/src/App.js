bc29422de41f5e234e8153c36aaf7be109c1f668
Alexander Moore <badi_moore@hotmail.com>
2022-01-25 13:43:49 +0200

    2.8 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 
-const Person = ({ person }) => <div>{person.name}</div>
+const Person = ({ person }) => <div>{person.name} {person.number}</div>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmitClick = evt => {
     evt.preventDefault()
@@ -14,24 +18,23 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return
     }
-    
+
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input value={newName} onChange={(evt) => setNewName(evt.target.value)} />
-        </div>
-        <div>
-          <button type="submit" onClick={handleSubmitClick}>add</button>
-        </div>
+        <div>name: <input value={newName} onChange={(evt) => setNewName(evt.target.value)} /></div>
+        <div>number: <input value={newNumber} onChange={(evt) => setNewNumber(evt.target.value)} /></div>
+        <div><button type="submit" onClick={handleSubmitClick}>add</button></div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => <Person person={person} key={person.name} />)}
